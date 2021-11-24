b322fc4277a653c1c17e50d4d27abc14d2d9b47c
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-24 12:52:21 -0600

    Added Index

--- fields/urls.py
+++ fields/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import WebDev, GameDev, Cybersecurity,ArtificialIntelligence,ContactInfo, Index
 
 urlpatterns = [
-    path('',Index, name="Index"),
+    path('Index',Index, name="Index"),
     path('WebDev',WebDev, name="WebDev"),
     path('GameDev',GameDev, name="GameDev"),
     path('Cybersecurity',Cybersecurity, name="Cybersecurity"),
