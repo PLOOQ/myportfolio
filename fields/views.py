d3763d3c350b84734b1e940d2b92722817da815c
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 16:10:54 -0600

    restored

--- fields/views.py
+++ fields/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def Index(request):
-    return render(request, "index.html")
+    return render(request, "fields/index.html")
 
 def WebDev(request):
     return render(request, "fields/webdev.html")
