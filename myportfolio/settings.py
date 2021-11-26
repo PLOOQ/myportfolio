2b3b3608a21c4d930a7db4bb0b2965982de29ff6
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 18:07:34 -0600

    added forward slash

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'myportfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), 
-                 os.path.join(BASE_DIR, 'fields','templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
