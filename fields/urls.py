7a20864ecbede8603389d032f04ca28b50b2afc0
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 20:48:02 -0600

    dash separated names

--- fields/urls.py
+++ fields/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('index',views.index, name="index"),
-    path('webdev',views.webdev, name="web-dev"),
-    path('gamedev',views.gamedev, name="game-dev"),
-    path('cybersecurity',views.cybersecurity, name="cyber-security"),
-    path('artificialintelligence',views.artificialintelligence, name="artificial-intelligence"),
-    path('contactinfo',views.contactInfo, name="contact-info"),
+    path('webdev',views.web_dev, name="web-dev"),
+    path('gamedev',views.game_dev, name="game-dev"),
+    path('cybersecurity',views.cyber_security, name="cyber-security"),
+    path('artificialintelligence',views.artificial_intelligence, name="artificial-intelligence"),
+    path('contactinfo',views.contact_info, name="contact-info"),
     
 ]
