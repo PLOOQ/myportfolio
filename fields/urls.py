721677ad798493e7a28729ac46ad83960812fa1a
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 19:56:25 -0600

    config

--- fields/urls.py
+++ fields/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import Webdev, GameDev, Cybersecurity,ArtificialIntelligence,ContactInfo, Index
 
 urlpatterns = [
-    path('',Index, name="index"),
-    path('webdev/',Webdev, name="webdev"),
+    path('webdev/',Index, name="index"),
+    path('',Webdev, name="webdev"),
     path('gamedev/',GameDev, name="gamedev"),
     path('cybersecurity/',Cybersecurity, name="cybersecurity"),
     path('artificialintelligence/',ArtificialIntelligence, name="artificialintelligence"),
