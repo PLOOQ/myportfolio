ccbe60eda1ff783e9346febb9becbbe83438e53e
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 21:12:53 -0600

    empty index

--- fields/urls.py
+++ fields/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index, name="index"),
+    path('',views.index, name="index"),
     path('devweb',views.dev_web, name="dev-web"),
     path('gamedev',views.game_dev, name="game-dev"),
     path('cybersecurity',views.cyber_security, name="cyber-security"),
