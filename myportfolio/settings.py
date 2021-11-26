5c99221799f9503eb7b04ec8a8b18f9dbe574f0d
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-25 18:26:23 -0600

    os path join staticfiles dirs

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -126,4 +126,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [BASE_DIR + '/static']
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
