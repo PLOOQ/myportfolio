93131838385f77fd9c0b1e37c223e5265e2d87c2
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-29 12:54:34 -0600

    Debug = False

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#r*v3r$wc*#udqadet2lj=9rz@sabyt99)y67+lryk&^n3dkkb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','mighty-myportfolio.herokuapp.com']
 
