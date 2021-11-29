83fb305e774d878ccf3727694d3f30aa4722411a
Pablo Loo <pabloloo@aggienetwork.com>
2021-11-29 14:25:59 -0600

    Debug = False

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#r*v3r$wc*#udqadet2lj=9rz@sabyt99)y67+lryk&^n3dkkb'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','mighty-myportfolio.herokuapp.com']
 
