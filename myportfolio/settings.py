44d10f25a3875740985c163e0ccd6fb4bc1a01f5
Pablo Loo <pabloloo@aggienetwork.com>
2023-05-08 17:12:55 -0500

    Vercel does not support dbsqlite3

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 
 # settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
