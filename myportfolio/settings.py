14335e5ce27bc073acb2a03822d95b6f3b529976
Pablo Loo <pabloloo@aggienetwork.com>
2023-05-08 13:30:04 -0500

    Static directory and database

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -76,14 +76,13 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # settings.py
-DATABASES = {}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
