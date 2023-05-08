7b6b28278d374dd1c2abc76719c287f0fdb04261
Pablo Loo <pabloloo@aggienetwork.com>
2023-05-08 13:19:03 -0500

    Cleaned up folders

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == "False")
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','mighty-myportfolio.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','mighty-myportfolio.herokuapp.com', 'eb-django-app-dev.elasticbeanstalk.com']
 
 
 
